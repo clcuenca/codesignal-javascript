2be8904e11a6c9f44b64b47375780b7c26082988
carloscuenca <cuenca.cl@gmail.com>
2020-05-20 10:28:16 -0700

    add countSumOfTwoRepresentations2

--- LoopTunnel.js
+++ LoopTunnel.js
@@ -1,3 +1,12 @@
+
+// -------------------------------------------------------------------
+// Finds the smallest factorial result relative to the given integer n
+//
+// Problem #25
+//
+// Author: Carlos L. Cuenca
+// Date: 05/19/2020
+
 function leastFactorial(n) {
 
     var factorial = 1
@@ -12,5 +21,31 @@ function leastFactorial(n) {
     }
 
     return result
+
+}
+
+// -------------------------------------------------------------------
+// Finds the smallest factorial result relative to the given integer n
+//
+// Problem #26
+//
+// Author: Carlos L. Cuenca
+// Date: 05/19/2020
+
+function countSumOfTwoRepresentations2(n, l, r) {
+
+	var count = 0
+
+	for(var index = l; index <= r; index++) {
+
+		if(index <= (n - index) && (n - index) <= r) {
+
+			count++
+
+		}
+
+	}
+
+	return count
 
 }
