5d626dcc1e9ed9070c9c4ff81e78cf0991680572
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-08 13:23:16 -0700

    implement largestNumber

--- IntroGates.js
+++ IntroGates.js
@@ -5,11 +5,29 @@
  *
  * @author Carlos L. Cuenca
  * @date 07/08/2020
- * @param int n The number to sum digits
- * @return int the sum of the number's digits
+ * @param {Number} n The number to sum digits
+ * @return {Number} The sum of the number's digits
  */
+
 function addTwoNumbers(n) {
 
 	return parseInt(n / 10) + parseInt(n % 10);
 
 }
+
+/**
+ * Returns the largest base 10 number given the amount of digits, n
+ *
+ * Problem #2
+ *
+ * @author Carlos L. Cuenca
+ * @date 07/08/2020
+ * @param {Number} the amount of digits the number should have
+ * @return {Number} The largest resulting number with n digits
+ */
+function largestNumber(n) {
+
+	return 10**n - 1;
+
+}
+
