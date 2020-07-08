cb7fc913580cad76dbe4ce80f4002f0f9259d2cd
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-08 13:17:29 -0700

    document addTwoNumbers

--- IntroGates.js
+++ IntroGates.js
@@ -1,4 +1,13 @@
-
+/**
+ * Returns the sum of the given number's digits
+ *
+ * Problem #1
+ *
+ * @author Carlos L. Cuenca
+ * @date 07/08/2020
+ * @param int n The number to sum digits
+ * @return int the sum of the number's digits
+ */
 function addTwoNumbers(n) {
 
 	return parseInt(n / 10) + parseInt(n % 10);
