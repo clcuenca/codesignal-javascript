375627c84170dc4e26bd00029c667df6951038fe
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-16 12:46:33 -0700

    implement knapsackLight

--- AtTheCrossroads.js
+++ AtTheCrossroads.js
@@ -12,9 +12,48 @@
  * @param number reward The amount of awarded experience
  * @return boolean value denoting if the next level has been reached
  */
- 
+
 function reachNextLevel(experience, threshold, reward) {
 
 	return (experience + reward) >= threshold;
+
+}
+
+/**
+ * Two weights, their values, & the maximum amount of weight that 
+ * can be carried, this method returns the maximum value of the weights
+ * 
+ * Problem #10
+ *
+ * @author: Carlos L. Cuenca
+ * @since: 07/16/2020
+ * @param number value1 The value of the first stone
+ * @param number weight1 The weight of the first stone
+ * @param number value2 The value of the second stone
+ * @param number weight2 The weight of the second stone
+ * @return number Value of the maximum amount of stones that can be carried
+ */
+
+function knapsackLight(value1, weight1, value2, weight2, maxW) {
+
+	if((weight1 + weight2) <= maxW) {
+
+		return value1 + value2;
+
+	}
+
+	if((weight1 > maxW) && (weight2 > maxW)) {
+
+		return 0;
+
+	}
+
+	if(value1 >= value2 || weight2 > maxW) {
+
+		return value1;
+
+	}
+
+	return value2;
 
 }
