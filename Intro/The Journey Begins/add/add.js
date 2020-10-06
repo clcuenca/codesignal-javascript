65c0f82650679570c1dd3d32fa6dcd33268b37d8
Carlos Cuenca <cuenca.cl@gmail.com>
2020-10-06 14:58:09 -0700

    add solution 'add' & 'centuryFromYear'

---
+++ Intro/The Journey Begins/add/add.js
@@ -0,0 +1,44 @@
+/**
+ * CodeSignal - Intro - The Journey Begins
+ * add
+ * Author: Carlos L. Cuenca
+ * Date: 10/06/20
+ */
+
+var readline = require('readline');
+var reader = readline.createInterface({
+
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
+
+});
+
+reader.on('line', function(line) {
+
+	var parameters = line.split(" ");
+
+	console.log(add(parseInt(parameters[0], 10), parseInt(parameters[1], 10)));
+
+});
+
+/**
+ * Adds two numbers & returns the result
+ * 
+ * Problem #9
+ *
+ * @author: Carlos L. Cuenca
+ * @since: 10/06/2020
+ * @param Number param1 The first number to add
+ * @param Number param2 The second number to add
+ * @return Number the result of the addition operation
+ */
+function add(param1, param2) {
+
+ 	return param1 + param2;
+
+}
+
+
+
+
